cef22002dd13709cd445f3d9162f6a531f6fd6d4
winterandrea <andrecasero@gmail.com>
2021-06-11 16:33:45 +0200

    animate schede

--- js/gsap_animation.js
+++ js/gsap_animation.js
@@ -5,8 +5,8 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#cloud", {scrollTrigger: {trigger:"#cloud",  start:"top center"}, duration: 3, x:-2100, y:600, rotate: -40})
+gsap.from("#cloud", {scrollTrigger: {trigger:"#cloud",  start:"top bottom"}, duration: 3, x:-2100, y:600, opacity:0, rotate: -40})
 
-gsap.from("#partner", {scrollTrigger: {trigger:"#partner", start:"top 50%"}, duration: 3, x:2100, rotate: 20})
+gsap.from("#partner", {scrollTrigger: {trigger:"#partner", start:"top 50%"}, duration: 3, x:2100, opacity:0, rotate: 20})
 
-gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top center"}, duration: 3, x:-1900, rotate: -20})
+gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top center"}, duration: 3, x:-1900, opacity:0, rotate: -20})
