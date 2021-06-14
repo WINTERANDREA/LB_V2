abdcd157ed5f4fcaab4c33b139f9ef41c238b7e2
winterandrea <andrecasero@gmail.com>
2021-06-14 19:58:58 +0200

    testo animato

--- js/gsap_animation.js
+++ js/gsap_animation.js
@@ -10,3 +10,5 @@ gsap.from("#cloud", {scrollTrigger: {trigger:"#cloud",  start:"top bottom"}, dur
 gsap.from("#partner", {scrollTrigger: {trigger:"#partner", start:"top 50%"}, duration: 3, x:2100, opacity:0, rotate: 20})
 
 gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top center"}, duration: 3, x:-1900, opacity:0, rotate: -20})
+
+
---
+++ js/main.js
@@ -0,0 +1,52 @@
+/* TESTO CORSIVO */
+
+window.onload = function(){
+var svgs = [
+    { file: "media/hero/testoCorsivoHero.svg", container: ".hero-testo" }
+  ];
+
+    svgs.forEach(loadSVG);
+  
+  function loadSVG(svg) {
+    
+    var request = new XMLHttpRequest();
+    request.open("GET", svg.file, true);
+    request.onload = function() {
+      
+      if (request.status >= 200 && request.status < 400) {
+        
+        insert(svg, request.responseText);
+      }
+    };
+    request.onerror = function() {
+      // You got a problem
+    };
+    request.send();
+  }
+
+  function insert(svg, data) {
+    
+    var container = document.querySelector(svg.container);
+    container.insertAdjacentHTML("beforeend", data);
+
+    // if (++loaded === total) startAnimation();
+    startAnimation()
+  }
+
+  function startAnimation(){
+    anime({
+      targets: '#testoCorsivo path',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'linear',
+      duration: 200,
+      delay: function (el, i) { return i * 200 },
+      loop: false,
+      complete: function(){
+        document.querySelector('#testoCorsivo').style.animation = 'fill-opacity 1s linear forwards'
+      }
+     
+    });
+  }
+
+}
+ 
