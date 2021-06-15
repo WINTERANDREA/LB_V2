3156cd20ef7954a6f145b723868e804064c60608
winterandrea <andrecasero@gmail.com>
2021-06-15 17:08:27 +0200

    icon footer

---
+++ js/icone_footer.js
@@ -0,0 +1,25 @@
+//Rotate footer icon
+
+
+const footerIcon = document.querySelector('.circle');
+
+observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      console.log('in the view');
+      anime({
+        targets: '.circle',
+        translateX: 400,
+        translateY:10,
+        rotate: '1turn',
+        duration: 8000,
+      });
+      observer.unobserve(entry.target);
+    } else {
+      console.log('out of view');
+    }
+  })
+  
+});
+
+observer.observe(footerIcon);
