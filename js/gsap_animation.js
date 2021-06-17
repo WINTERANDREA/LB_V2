263af42b102b5dd9e96af58ae7c36ebf62deb519
winterandrea <andrecasero@gmail.com>
2021-06-17 12:40:10 +0200

    ddd

--- js/gsap_animation.js
+++ js/gsap_animation.js
@@ -2,18 +2,18 @@ let tl = gsap.timeline({scrollTrigger:{
   trigger:".about-testo",
   start:"top bottom",
 }})
-.from(".about-testo", {y:300, opacity:0, duration:2})
-.from(".btn-about", { opacity:0.2, delay:.5, scale: 0, ease:"back", duration:1})
+.from(".about-testo", {y:200, opacity:0, duration:2})
+.from(".btn-about", { opacity:0.2, scale: 0, ease:"back", duration:1})
 
 
 /* SCHEDE */
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#cloud", {scrollTrigger: {trigger:"#cloud",  start:"top center"}, duration: 2, x:-2100, y:600, opacity:0, rotate: -40, scrub:.5})
+gsap.from("#cloud", {scrollTrigger: {trigger:"#cloud",  start:"top bottom"}, duration: 3, x:-700, y:200, opacity:0, rotate: -40})
 
-gsap.from("#partner", {scrollTrigger: {trigger:"#partner", start:"top 50%"}, duration: 2, x:2100, opacity:0, rotate: 10, scrub:.5})
+gsap.from("#partner", {scrollTrigger: {trigger:"#partner", start:"top center"}, duration: 3, x:700, opacity:0, rotate: 10})
 
-gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top center"}, duration: 2, x:-1900, opacity:0, rotate: -20, scrub:.5})
+gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top center"}, duration: 3, x:-900, opacity:0, rotate: -20})
 
 
