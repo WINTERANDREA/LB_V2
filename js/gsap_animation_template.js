ac9d6ddc2eff8e201e5a952a2d43774995a3c951
winterandrea <andrecasero@gmail.com>
2021-06-30 17:41:37 +0200

    df

--- js/gsap_animation_template.js
+++ js/gsap_animation_template.js
@@ -11,6 +11,7 @@ gsap.from(".svg-1", {scrollTrigger: {trigger:".paragrafo-1-svg", start:"top 80%"
 gsap.from(".paragrafo-2", {scrollTrigger: {trigger:".paragrafo-2", start:"top 80%"}, duration: 1, y:200, opacity:0})
 gsap.from(".paragrafo-2-svg", {scrollTrigger: {trigger:".paragrafo-2-svg", start:"top 80%"}, duration: 1, x:200, opacity:0})
 gsap.from(".paragrafo-titolo", {scrollTrigger: {trigger:".paragrafo-2-svg", start:"top 80%"}, duration: 1, x:200, opacity:0})
+gsap.from(".paragrafo-titolo-svg", {scrollTrigger: {trigger:".paragrafo-2-svg", start:"top 80%"}, duration: 1, x:200, opacity:0})
 gsap.from(".svg-2", {scrollTrigger: {trigger:".paragrafo-2-svg", start:"top 80%"}, duration: 1, x:-200, opacity:0})
 gsap.from(".paragrafo-3", {scrollTrigger: {trigger:".paragrafo-3", start:"top 80%"}, duration: 1, y:200, opacity:0})
 gsap.from(".paragrafo-3-svg", {scrollTrigger: {trigger:".paragrafo-3-svg", start:"top 80%"}, duration: 1, x:-200, opacity:0})
@@ -22,6 +23,8 @@ gsap.from("#myform", {scrollTrigger: {trigger:"#myform", start:"top 80%"}, durat
 gsap.from(".btn-template", {scrollTrigger: {trigger:".btn-template", start:"top 80%"}, duration: 1, y:200, opacity:0})
 gsap.from(".link-sito", {scrollTrigger: {trigger:".link-sito", start:"top 80%"}, duration: 1, y:200, opacity:0})
 gsap.from("#referenze", {scrollTrigger: {trigger:"#referenze", start:"top 50%"}, duration: 1, x:-900, opacity:0, rotate: -10})
+
+
 
 
 
